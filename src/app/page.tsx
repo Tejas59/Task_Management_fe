cac33d2488320d49fab56b29e7477be21482aa28
Tejas59 <57625719+Tejas59@users.noreply.github.com>
2025-05-28 23:09:26 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import NewTaskModal from "@/components/NewTaskModal";
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <NewTaskModal />
+    </div>
+  );
+}
---
+++ src/components/NewTaskModal.tsx
@@ -0,0 +1,12 @@
+const NewTaskModal = () => {
+  return (
+    <div className="h-[600px] w-[400px] bg-yellow-50 flex justify-center p-[30px]">
+      <div className="flex justify-between w-full">
+        <div>New Task</div>
+        <div>open/closed</div>
+      </div>
+    </div>
+  );
+};
+
+export default NewTaskModal;
